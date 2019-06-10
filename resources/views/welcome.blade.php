7eaefbafade5b2e600778edad186304d73c65dc8
mdzroo <contact@mdz.ro>
2019-06-10 23:17:58 +0100

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Hello
+                    Hello ~
                 </div>
 
                 <div class="links">
